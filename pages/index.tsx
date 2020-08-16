55ea9b57e6238d75ca929099f257136beb870fba
luohuidong <luohuidong01@126.com>
2020-08-17 01:51:01 +0800

    update

---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { GetStaticProps } from "next";
+
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/dates";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <section className={utilStyles.headingMd} style={{ textAlign: "center" }}>
+        <p>职业吹牛逼，业余写代码</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import { GetStaticProps, GetStaticPaths } from "next";
+
+import postsStyles from "styles/post.module.scss";
+
+import { getAllPostIds, getPostData } from "lib/posts";
+import Layout from "components/layout";
+import Date from "components/dates";
+import prismjs from "lib/prismjs";
+
+export default function Post({ postData, html }) {
+  console.log("Post -> html", html);
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <article className={postsStyles.artical}>
+        <h1>{postData.title}</h1>
+
+        <div className={postsStyles.date}>
+          <Date dateString={postData.date} />
+        </div>
+
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id);
+  console.log("getStaticProps -> postData", postData);
+  const html = prismjs();
+  console.log("getStaticProps -> html", html);
+  return {
+    props: {
+      postData,
+      html,
+    },
+  };
+};
