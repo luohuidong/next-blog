ae856f01c361866a5647b5d54a3be71261ab617d
luohuidong <luohuidong01@126.com>
2020-12-29 16:05:45 +0800

    提取底部内容

---
+++ components/LayoutFooter/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+export default function LayoutFooter() {
+  return (
+    <footer className={styles.footer}>
+      <a href="https://beian.miit.gov.cn/">网站备案号：粤ICP备18108480号</a>
+    </footer>
+  );
+}
