f86a33724d89456f10ea482551c4e417d5a60d99
luohuidong <luohuidong01@126.com>
2021-01-27 17:37:33 +0800

    新增目录根据滚动位置进行高亮

--- components/PostTOC/index.tsx
+++ components/PostTOC/index.tsx
@@ -1,8 +1,27 @@
 import React, { useEffect, useState } from "react";
+import classnames from "classnames";
 
 import styles from "./index.module.scss";
 
 export default function PostTOC() {
+  /**
+   * 获取文章所有标题
+   * @param cb
+   */
+  function getHeadings(cb: (headingNode: HTMLElement) => void) {
+    const postContainer = document.getElementById("post");
+    const headerType = new Set(["h2", "h3", "h4", "h5", "h6"]);
+    for (let child of postContainer.childNodes) {
+      // 筛选指定类型的 header 元素
+      if (
+        child.nodeType === 1 &&
+        headerType.has(child.nodeName.toLowerCase())
+      ) {
+        cb(child as HTMLElement);
+      }
+    }
+  }
+
   interface Header {
     /** header 元素的 id 值 */
     elementId: string;
@@ -14,32 +33,73 @@ export default function PostTOC() {
   const [headerData, setHeaderData] = useState<Header[]>([]);
   useEffect(() => {
     const headerData: Header[] = [];
-    const postContainer = document.getElementById("post");
-    const headerType = new Set(["h2", "h3", "h4", "h5", "h6"]);
     const regexp = /^h(\d)$/;
-    for (let child of postContainer.childNodes) {
-      // 筛选指定类型的 header 元素
-      if (
-        child.nodeType === 1 &&
-        headerType.has(child.nodeName.toLowerCase())
-      ) {
-        const { id, firstChild, tagName } = child as HTMLElement;
-        const result = regexp.exec(tagName.toLowerCase());
-        headerData.push({
-          elementId: id,
-          text: firstChild.nodeValue,
-          level: Number(result[1]),
-        });
-      }
-    }
+    getHeadings((heading) => {
+      const result = regexp.exec(heading.tagName.toLowerCase());
+      headerData.push({
+        elementId: heading.id,
+        text: heading.firstChild.nodeValue,
+        level: Number(result[1]),
+      });
+    });
+    console.log(
+      "🚀 ~ file: index.tsx ~ line 38 ~ useEffect ~ headerData",
+      headerData
+    );
+
     setHeaderData(headerData);
   }, []);
+
+  const [highLightHeadingId, setHighLightHeadingId] = useState<string>("");
+  useEffect(
+    function () {
+      function throttle(fn: Function, interval) {
+        let timer: NodeJS.Timeout = null;
+
+        return function () {
+          if (timer) {
+            return;
+          }
+          timer = setTimeout(function () {
+            fn.call(this, arguments);
+            timer = null;
+          }, interval);
+        };
+      }
+
+      function handleScroll() {
+        const firstHead = headerData[0];
+        let id = firstHead ? firstHead.elementId : "";
+        getHeadings((heading) => {
+          const top = heading.getBoundingClientRect().top;
+          if (top <= 50) {
+            id = heading.id;
+          }
+        });
+        console.log("id", id);
+        setHighLightHeadingId(id);
+      }
+      const handler = throttle(handleScroll, 100);
+      window.addEventListener("scroll", handler);
+
+      return () => {
+        window.removeEventListener("scroll", handler);
+      };
+    },
+    [headerData]
+  );
 
   return (
     <ul className={styles.toc}>
       {headerData.map((header) => (
         <a key={header.elementId} href={`#${header.elementId}`}>
-          <li className={styles[`level${header.level}`]}>{header.text}</li>
+          <li
+            className={classnames(styles[`level${header.level}`], {
+              [styles.highLight]: highLightHeadingId === header.elementId,
+            })}
+          >
+            {header.text}
+          </li>
         </a>
       ))}
     </ul>
