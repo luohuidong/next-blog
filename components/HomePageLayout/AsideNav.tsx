7f5a2d3d5dabe6317f26b189e70a6b2d54385e74
luohuidong <luohuidong01@126.com>
2021-02-03 14:01:06 +0800

    首页侧边栏添加分类导航链接
    test

--- components/HomePageLayout/AsideNav.tsx
+++ components/HomePageLayout/AsideNav.tsx
@@ -3,30 +3,38 @@ import Link from "next/link";
 
 import styles from "./AsideNav.module.scss";
 
+function NavLink(props: { href: string; iconSrc: string; text: string }) {
+  return (
+    <Link href={props.href}>
+      <a style={{ width: 100, marginBottom: 20 }}>
+        <img
+          src={props.iconSrc}
+          alt=""
+          style={{
+            width: 30,
+            height: 30,
+          }}
+        />
+        <span
+          style={{
+            fontWeight: "bold",
+            fontSize: 18,
+            color: "#262626",
+            marginLeft: 20,
+          }}
+        >
+          {props.text}
+        </span>
+      </a>
+    </Link>
+  );
+}
+
 export default function AsideNav() {
   return (
     <div className={styles.nav}>
-      <Link href="/tags">
-        <a style={{ width: 100 }}>
-          <img
-            src="/icons/bookmark.svg"
-            alt=""
-            style={{
-              width: 30,
-              height: 30,
-            }}
-          />
-          <span
-            style={{
-              fontWeight: "bold",
-              fontSize: 18,
-              color: "#262626",
-            }}
-          >
-            标签
-          </span>
-        </a>
-      </Link>
+      <NavLink href="/categories" iconSrc="/icons/category.png" text="分类" />
+      <NavLink href="/tags" iconSrc="icons/tags.png" text="标签" />
     </div>
   );
 }
